923922eb9f3d5369c64dfc81ea4e4dfc06b5ca11
guswn2521 <>
2021-01-31 15:51:40 +0900

    state
    class extends React.Component
    1. state : 변하는 데이터의 객체
    2. react는 모든 class component의 render method를 실행함.
    *render : screen에 보여줌.
    3. setState() : 새로운 state값을 지정, render()실행.

--- src/App.js
+++ src/App.js
@@ -1,81 +1,28 @@
 import React from "react";
 import PropTyes from "prop-types";
 
-// props = {fav}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-      rating:5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-      rating:5
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-      rating:5
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-      rating:4.7
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating:4.7
+class App extends React.Component {
+  state = {
+    count:0
   }
-];
-
-function Food({name, picture, rating}){
-  return(
+  add = () =>{
+    this.setState({count:1});
+    console.log("add");
+  };
+  minus = () =>{
+    this.setState({count:-1});
+    console.log("minus");
+  };
+  render() {
+    return (
     <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}></img>
-      
+      <h1>The number is : {this.state.count} </h1>
+      <button onClick = {this.add}>Add</button>
+      <button onClick = {this.minus}>Minus</button>
     </div>
-  )
-}
+    )
+  }
 
-//description of props
-Food.propTypes = {
-  name:PropTyes.string.isRequired,
-  picture:PropTyes.string.isRequired,
-  rating:PropTyes.number.isRequired
-};
-
-function renderFood(dish){
-  console.log(dish);
-  return <Food name={dish.name} picture={dish.image}/>
-}
-
-function App() {
-  return (
-    // <div>
-    //   {foodILike.map(renderFood)}
-    // </div>
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}></Food>
-      ))
-      }
-    </div>
-  );
 }
 
 export default App;
