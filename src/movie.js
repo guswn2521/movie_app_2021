0fb716b7db2c7f3b41c45e8af428b6666442f5a9
guswn2521 <>
2021-01-31 21:54:08 +0900

    gh-pages
    npm i gh-pages
    git remote -v
    npm i gh-pages
    
    package.json - script추가
    npm run build
    npm run deploy

--- src/movie.js
+++ src/movie.js
@@ -1,33 +1,34 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTyeps from "prop-types";
 import "./Movie.css";
 
-function Movie({title,summary,poster,year,genres}){
+
+function Movie({ year, title, summary, poster, genres}){
     return (
     <div className="movie">
-        <img src={poster} alt={title} title={title}></img>
-        <div className="movie__data">
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <ul className="movie__genres">{genres.map((genre, index) => (
-                <li key={index} className="genres__genre">
-                    {genre}
-                </li>
-            ))}
-            </ul>
-            <p className="movie__summary">{summary.slice(0,180)}...</p>
-        </div>
+    <img src={poster} alt={title} title={title}></img>
+     <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+            {genres.map((genre, index) => 
+            <li key={index} className="genres__genre">
+                {genre}
+            </li>)}
+        </ul>
+        <p className="movie__summary">{summary.slice(0,180)}...</p>
+     </div>
     </div>
     )
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-    year: PropTypes.number.isRequired,
-    genres:PropTypes.arrayOf(PropTypes.string).isRequired
-};
+    id: PropTyeps.number.isRequired,
+    year: PropTyeps.number.isRequired,
+    title: PropTyeps.string.isRequired,
+    summary: PropTyeps.string.isRequired,
+    poster: PropTyeps.string.isRequired,
+    genres: PropTyeps.arrayOf(PropTyeps.string).isRequired
+}
 
 export default Movie;
