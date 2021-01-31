afd9aafeb85065e276bdcdf8d5fbffaa989067ad
guswn2521 <>
2021-01-31 16:06:46 +0900

    setState()
    1. setState() : virtualDom-> 변하는 부분만 바꿈. 깜박이지 않음.
    setState를 호출할 때마다 react는 새로운 state와 함께 render()호출함.
    2. setState(current => ({key : current.key}))
    current대신 아무거나 써도됨.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class App extends React.Component {
     count:0
   }
   add = () =>{
-    this.setState({count:1});
+    this.setState(current => ({count: current.count +1}));
     console.log("add");
   };
   minus = () =>{
-    this.setState({count:-1});
+    this.setState(current => ({count: current.count - 1}));
     console.log("minus");
   };
   render() {
